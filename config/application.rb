a0b6d539b389f04decb5a29c71592a6fd8e71a65
nooky123 <qzhoi86@gmail.com>
2013-01-02 16:33:37 +0800

    added assets

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module Drugscomparison
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['highcharts.js', 'jquery-rails.js']
   end
 end
