044b066699e22aff574b9712e0316b320ca98a58
nooky123 <qzhoi86@gmail.com>
2013-01-10 11:05:27 +0800

    completed a generic fetch all review method taking in an array of hash

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -30,9 +30,18 @@ class ConditionsController < ApplicationController
   @condition=Condition.find(params[:id])
   @drugs=@condition.drugs.scoped
   @generate_colors=Array.new
+  @update_values=Hash.new{|hash, key| hash[key] = Hash.new{|hash, key| hash[key] = Array.new}}
       @drugs.each do  |drug|
         @generate_colors.push('#E238EC','#8AFB17' ,'#736AFF')    #set the colors here then pass to the javascript
+        (@update_values["1"]["all"]).push(@condition.avg_eff(drug.id), @condition.avg_eou(drug.id) ,@condition.avg_sat(drug.id))
       end
+
+  #updated_values{
+  #"maleonly"=>[1,2,3,4,5,6],
+  #"all"=>[1..6],
+  #"agegrp1"=>[1..6]
+  #}
+
   respond_to do |format|
     format.js
   end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -23,26 +23,79 @@ class Condition < ActiveRecord::Base
     return appended_drug
   end
 
-  def get_all_reviews (for_drug_id)
-    Review.joins(:drug,:user).where(:drug_id=>for_drug_id)
+  def get_all_reviews (options = {})
+    #options[:for_drug_id]||options[:for_drug_id]=1
+    query_record=Review.joins(:drug,:user).where(:drug_id=>options[:for_drug_id])  #always search with a drug id
+     # add in other search conditions depending on what options were passed in
+    options.each do |key,value|
+      if key==:gender
+        query_record=query_record.where(:users=>{:gender=>value})
+        next
+      end
+      if key ==:age
+        query_record=query_record.where(:users=>{:age=>value})
+        next
+      end
+      if key ==:location
+        query_record=query_record.where(:users=>{:location=>value})
+        next
+      end
+      if key ==:ethnicity
+        query_record=query_record.where(:users=>{:ethnicity=>value})
+        next
+      end
+      if key ==:weight
+        query_record=query_record.where(:users=>{:weight=>value})
+        next
+      end
+      if key ==:smoking_status
+        query_record=query_record.where(:users=>{:smoking_status=>value})
+        next
+      end
+    end
 
+    return query_record
   end
+
   def get_male_reviews (for_drug_id)
     #@male=Array.new
     Review.joins(:drug,:user).where(:users=>{:gender=>'Male'},:drug_id=>for_drug_id)
 
   end
-  def get_male_reviews_counts (for_drug_id)
-    #@male=Array.new
-    Review.joins(:drug,:user).where(:users=>{:gender=>'Male'},:drug_id=>for_drug_id).counts
-  end
+
 
   def get_female_reviews (for_drug_id)
     #@male=Array.new
     Review.joins(:drug,:user).where(:users=>{:gender=>'Female'},:drug_id=>for_drug_id)
   end
-  def get_female_reviews_counts (for_drug_id)
-    #@male=Array.new
-    Review.joins(:drug,:user).where(:users=>{:gender=>'Female'},:drug_id=>for_drug_id).counts
+
+  # avg_* methods are for "all" tabs
+  def avg_eff(drugid)
+    score1=self.get_all_reviews(:for_drug_id=>drugid).where("effectiveness=1").count
+    score2=self.get_all_reviews(:for_drug_id=>drugid).where("effectiveness=2").count
+    score3=self.get_all_reviews(:for_drug_id=>drugid).where("effectiveness=3").count
+    score4=self.get_all_reviews(:for_drug_id=>drugid).where("effectiveness=4").count
+    score5=self.get_all_reviews(:for_drug_id=>drugid).where("effectiveness=5").count
+    sum=Float(self.get_all_reviews(:for_drug_id=>drugid).count)
+    weighted_average=((1*score1)+(2*score2)+(3*score3)+(4*score4)+(5*score5))/sum
   end
+  def avg_eou(drugid)
+    score1=self.get_all_reviews(:for_drug_id=>drugid).where("ease_of_use=1").count
+    score2=self.get_all_reviews(:for_drug_id=>drugid).where("ease_of_use=2").count
+    score3=self.get_all_reviews(:for_drug_id=>drugid).where("ease_of_use=3").count
+    score4=self.get_all_reviews(:for_drug_id=>drugid).where("ease_of_use=4").count
+    score5=self.get_all_reviews(:for_drug_id=>drugid).where("ease_of_use=5").count
+    sum=Float(self.get_all_reviews(:for_drug_id=>drugid).count)
+    weighted_average=((1*score1)+(2*score2)+(3*score3)+(4*score4)+(5*score5))/sum
+  end
+  def avg_sat(drugid)
+    score1=self.get_all_reviews(:for_drug_id=>drugid).where("satisfactory=1").count
+    score2=self.get_all_reviews(:for_drug_id=>drugid).where("satisfactory=2").count
+    score3=self.get_all_reviews(:for_drug_id=>drugid).where("satisfactory=3").count
+    score4=self.get_all_reviews(:for_drug_id=>drugid).where("satisfactory=4").count
+    score5=self.get_all_reviews(:for_drug_id=>drugid).where("satisfactory=5").count
+    sum=Float(self.get_all_reviews(:for_drug_id=>drugid).count)
+    weighted_average=((1*score1)+(2*score2)+(3*score3)+(4*score4)+(5*score5))/sum
+  end
+
 end
