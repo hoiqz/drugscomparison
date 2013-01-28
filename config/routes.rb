ad9913156878ac1cfe496c77796528c28dfa23b8
nooky123 <qzhoi86@gmail.com>
2013-01-28 09:54:14 +0800

    fixed assests

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,7 @@ Drugscomparison::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'drugs#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
