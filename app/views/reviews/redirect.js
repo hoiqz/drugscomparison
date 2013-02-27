6a4f3d8042f64d918bc5256d1c557cb5b66cb3b8
nooky123 <qzhoi86@gmail.com>
2013-02-27 17:07:40 +0800

    completed modal review submission

--- app/views/reviews/redirect.js
+++
@@ -1,5 +0,0 @@
-$('#myModal .modal-body').html("Review created successfully!")
-$('#newreviewform').submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-});
