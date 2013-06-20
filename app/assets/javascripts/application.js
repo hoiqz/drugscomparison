e9cbe0b811863dfb1721a63c43126d217c7b4195
nooky123 <qzhoi86@gmail.com>
2013-06-20 11:14:04 +0800

    trying to get this asset to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,9 @@
 //= require highcharts-more
 //= require exporting
 //= require jqcloud-1.0.2.min
+//= require footable
+//= require footablesortable
+//= require footablefilter
 //= require_tree .
 
 $(function () {
