c60033283a9ad318fea23176f3e0391154bc455c
nooky123 <qzhoi86@gmail.com>
2013-07-12 16:10:16 +0800

    condition show page fixes. finished pie graph resizing. doesnt flow out as much as before

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -13,44 +13,36 @@ class ConditionsController < ApplicationController
     @condition=Condition.find(params[:id])
     @drugs=@condition.drugs
     @number_of_drugs=@drugs.count
-    @generate_colors=Array.new
-    @drugs.each do  |drug|
-      @generate_colors.push('#E238EC','#8AFB17' ,'#736AFF')    #set the colors here then pass to the javascript
-    end
-
-    if params[:conditions]
-      @optionshash=params
-    end
 
     # this is for the tables view
-    if params[:button]
-    @metric=Hash.new
-    @conmetric=Conditionmetric.find_all_by_condition(@condition.name)
-    @conmetric.each do |item|
-      review_count=Drug.find_by_brand_name(item.drug).reviews_count.to_f
-      if params[:button]=="sat-table"
-        @metric[item.drug] ={
+    if (params[:button]=="sat-table" || params[:button]=="eff-table" || params[:button]=="eou-table" )
+      @metric=Hash.new
+      @conmetric=Conditionmetric.find_all_by_condition(@condition.name)
+      @conmetric.each do |item|
+        review_count=Drug.find_by_brand_name(item.drug).reviews_count.to_f
+        if params[:button]=="sat-table"
+          @metric[item.drug] ={
               :good=> (item.sat_good)/review_count,
               :avg=> (item.sat_avg)/review_count,
               :bad=>(item.sat_bad)/review_count,
               :counts=>review_count.to_i
-      }
-      elsif params[:button]=="eff-table"
-        @metric[item.drug] ={
-                :good=> (item.eff_good)/review_count,
-                :avg=> (item.eff_avg)/review_count,
-                :bad=>(item.eff_bad)/review_count,
-                :counts=>review_count.to_i
-        }
-      elsif params[:button]=="eou-table"
-        @metric[item.drug] ={
-                :good=> (item.eou_good)/review_count,
-                :avg=> (item.eou_avg)/review_count,
-                :bad=>(item.eou_bad)/review_count,
-                :counts=>review_count.to_i
-        }
+          }
+        elsif params[:button]=="eff-table"
+          @metric[item.drug] ={
+              :good=> (item.eff_good)/review_count,
+              :avg=> (item.eff_avg)/review_count,
+              :bad=>(item.eff_bad)/review_count,
+              :counts=>review_count.to_i
+          }
+        elsif params[:button]=="eou-table"
+          @metric[item.drug] ={
+              :good=> (item.eou_good)/review_count,
+              :avg=> (item.eou_avg)/review_count,
+              :bad=>(item.eou_bad)/review_count,
+              :counts=>review_count.to_i
+          }
+        end
       end
-    end
     end
     #end table view
 
@@ -76,34 +68,43 @@ class ConditionsController < ApplicationController
     # end infograph stuff
 
     # for pie charts
-    @generate_colors=Array.new
-    @label='Nil'
-    if params[:conditions]
-      @label=''
-      labels=params.except(:utf8,:amp,:commit,:action,:controller,:page,:type,:conditions,:id,:button,:drug_id)
-      labels.each_pair do |key,value|
-        @label<< "#{key.capitalize} : #{value} , "
+    if (params[:button]=="satisfactory_view" || params[:button]=="effectiveness_view" || params[:button]=="eou_view" ||
+        params[:button]=="effectlink" || params[:button]=="eoulink" || params[:button]=="satisfylink")
+      @generate_colors=Array.new
+      @label='Nil'
+      if params[:conditions].present?
+        @label=''
+        labels=params.except(:utf8,:amp,:commit,:action,:controller,:page,:type,:conditions,:id,:button,:drug_id)
+        labels.each_pair do |key,value|
+          @label<< "#{key.capitalize} : #{value} , "
+        end
+        @label='Nil' if @label.blank?
       end
-      @label='Nil' if @label.blank?
-    end
 
-    @review_options=params
-    @update_values = Hash.new{|hash, key| hash[key] = Array.new}
-    @drugs.each do  |drug|
-      @review_options[:for_drug_id]=drug.id
-      @related_reviews=@condition.get_all_reviews(@review_options)
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
-      (@update_values[drug]).push(@condition.eou_score1(@related_reviews).round(2), @condition.eou_score2(@related_reviews).round(2),@condition.eou_score3(@related_reviews).round(2),@condition.eou_score4(@related_reviews).round(2),@condition.eou_score5(@related_reviews).round(2))
+      @review_options=params
+      @update_values = Hash.new{|hash, key| hash[key] = Array.new}
+      @drugs.each do  |drug|
+        @review_options[:for_drug_id]=drug.id
+        @related_reviews=@condition.get_all_reviews(@review_options)
+        @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
+        if(params[:button]=="satisfactory_view" || params[:button]=="satisfylink")
+          (@update_values[drug]).push(@condition.sat_score1(@related_reviews).round(2), @condition.sat_score2(@related_reviews).round(2),@condition.sat_score3(@related_reviews).round(2),@condition.sat_score4(@related_reviews).round(2),@condition.sat_score5(@related_reviews).round(2))
+        elsif(params[:button]=="effectiveness_view" || params[:button]=="effectlink")
+          (@update_values[drug]).push(@condition.eff_score1(@related_reviews).round(2), @condition.eff_score2(@related_reviews).round(2) ,@condition.eff_score3(@related_reviews).round(2),@condition.eff_score4(@related_reviews).round(2),@condition.eff_score5(@related_reviews).round(2))
+        else
+          (@update_values[drug]).push(@condition.eou_score1(@related_reviews).round(2), @condition.eou_score2(@related_reviews).round(2),@condition.eou_score3(@related_reviews).round(2),@condition.eou_score4(@related_reviews).round(2),@condition.eou_score5(@related_reviews).round(2))
+        end
+      end
     end
 
     respond_to do |format|
-          format.js
-          format.html # show.html.erb
-          format.json { render json: @drug }
+      format.js
+      format.html # show.html.erb
+      format.json { render json: @drug }
 
-      end
-    #  format.js
     end
+    #  format.js
+  end
 
   def format2hash_string(string)
     stringhash={}
@@ -144,146 +145,14 @@ class ConditionsController < ApplicationController
     end
   end
 
-  def effectiveness_view
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
 
-    #for the condition label
-    @label='Nil'
-    if params[:conditions]
-      @label=''
-      labels=params.except(:utf8,:amp,:commit,:action,:controller,:page,:type,:conditions,:id)
-      labels.each_pair do |key,value|
-        @label<< "#{key.capitalize} : #{value} , "
-      end
-      @label='Nil' if @label.blank?
-    end
-
-    @review_options=params
-    @update_values = Hash.new{|hash, key| hash[key] = Array.new}
-    @drugs.each do  |drug|
-      @review_options[:for_drug_id]=drug.id
-      @related_reviews=@condition.get_all_reviews(@review_options)
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')     #set the colors here then pass to the javascript
-      (@update_values[drug]).push(@condition.eff_score1(@related_reviews).round(2), @condition.eff_score2(@related_reviews).round(2) ,@condition.eff_score3(@related_reviews).round(2),@condition.eff_score4(@related_reviews).round(2),@condition.eff_score5(@related_reviews).round(2))
-    end
-    respond_to do |format|
-      #format.html
-      format.js
-    end
-  end
-
-  def eou_view
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
-
-    #for the condition label
-    @label='Nil'
-    if params[:conditions]
-      @label=''
-      labels=params.except(:utf8,:amp,:commit,:action,:controller,:page,:type,:conditions,:id)
-      labels.each_pair do |key,value|
-        @label<< "#{key.capitalize} : #{value} , "
-      end
-      @label='Nil' if @label.blank?
-    end
-
-    @review_options=params
-    @update_values = Hash.new{|hash, key| hash[key] = Array.new}
-    @drugs.each do  |drug|
-      @review_options[:for_drug_id]=drug.id
-      @related_reviews=@condition.get_all_reviews(@review_options)
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
-      (@update_values[drug]).push(@condition.eou_score1(@related_reviews).round(2), @condition.eou_score2(@related_reviews).round(2),@condition.eou_score3(@related_reviews).round(2),@condition.eou_score4(@related_reviews).round(2),@condition.eou_score5(@related_reviews).round(2))
-    end
-    respond_to do |format|
-      #format.html
-      format.js
-    end
-  end
-
-  def satisfactory_view
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
-
-    #for the condition label
-    @label='Nil'
-    if params[:conditions]
-      @label=''
-      labels=params.except(:utf8,:amp,:commit,:action,:controller,:page,:type,:conditions,:id)
-      labels.each_pair do |key,value|
-        @label<< "#{key.capitalize} : #{value} , "
-      end
-      @label='Nil' if @label.blank?
-    end
-
-    @review_options=params
-    @update_values = Hash.new{|hash, key| hash[key] = Array.new}
-    @drugs.each do  |drug|
-      @review_options[:for_drug_id]=drug.id
-      @related_reviews=@condition.get_all_reviews(@review_options)
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
-      (@update_values[drug]).push(@condition.sat_score1(@related_reviews).round(2), @condition.sat_score2(@related_reviews).round(2),@condition.sat_score3(@related_reviews).round(2),@condition.sat_score4(@related_reviews).round(2),@condition.sat_score5(@related_reviews).round(2))
-    end
-    respond_to do |format|
-      #format.html
-      format.js
-    end
-  end
-
-  ##DEPRECEATED METHODS
-  def gender_distinction
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs
-    respond_to do |format|
-      format.js
-    end
-  end
-  def by_gender_all_effectiveness
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
-    @drugs.each do  |drug|
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
-    end
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def by_gender_male_effectiveness
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
-    @drugs.each do  |drug|
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')      #set the colors here then pass to the javascript
-    end
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def by_gender_female_effectiveness
-    @condition=Condition.find(params[:id])
-    @drugs=@condition.drugs.scoped
-    @generate_colors=Array.new
-    @drugs.each do  |drug|
-      @generate_colors.push('#C11B17','#FF9999' , '#FFCC33','#99FF33' , '#009900')     #set the colors here then pass to the javascript
-    end
-    respond_to do |format|
-      format.js
-    end
-  end
 
   private
 
   def resolve_layout
     case action_name
       #when "new", "create"
-       # "some_layout"
+      # "some_layout"
       when "show"
         "condition_layout"
       else
