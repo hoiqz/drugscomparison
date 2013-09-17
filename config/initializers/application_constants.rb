bc9afa9b89aa7fd261a9242ce1be6d23889072ae
nooky123 <qzhoi86@gmail.com>
2013-09-17 16:15:06 +0800

    testing domain name

--- config/initializers/application_constants.rb
+++ config/initializers/application_constants.rb
@@ -1,2 +1,2 @@
-DOMAIN_NAMES = {"development" => "", "production" =>  "www.choosemydrug.com", "test" => ""}
+DOMAIN_NAMES = {"development" => "", "production" =>  "http://www.choosemydrug.com", "test" => ""}
 DOMAIN_NAME = DOMAIN_NAMES[Rails.env]
